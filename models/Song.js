e71d443f9838fc0cdacdc6e58760f295ea5ad855
Shaienn <shaienn@mail.ru>
2016-05-21 10:28:19 +0300

    Fix missed dependencies.

--- models/Song.js
+++ models/Song.js
@@ -17,7 +17,7 @@ Song.add({
     singer: {type: Types.Relationship, ref: 'Singer', index: true, initial: true},
     name: {type: String, required: true, initial: true},
     gsid: {type: Number, noedit: true, default: 0, watch: 'singer name state text', value: save_song},
-    text: {type: Types.Song},
+    text: {type: Types.Text},
     state: {type: Types.Select, options: 'committed, approved', default: 'committed', index: true},
     author: {type: Types.Relationship, ref: 'User', index: true},
     variants: {type: Types.Relationship, ref: 'SongVariant', many: true, hidden: true}
